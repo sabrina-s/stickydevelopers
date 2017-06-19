bedc9e36800c097a600e4523991cdb8f29f487d5
sabrina-s <tesmemepasla@gmail.com>
2017-06-19 16:10:40 +0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/pgadmin', as: 'rails_admin'
-  devise_for :admins
+
   devise_for :users
+  namespace :user do
+    resources :products, only: [:index, :show]
+    resource :cart, except: [:new, :create]
+    resources :orders
+  end
+
+  devise_for :admins
+  namespace :admin do
+    resources :products
+  end
+
+  # For Guests
+  resources :products, only: [:index, :show]
+  resource :cart, except: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  
+
 
 end
