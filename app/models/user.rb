cf34f064ad058eca8b00bc1b516bb5272f90b422
nndd91 <nndd91@hotmail.com>
2017-06-20 12:06:46 +0800

    Fix wrongly labeled name in users.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
-        user.first_name = data["first_name"] if user.first_name.blank?
+        user.first_name = data["name"] if user.first_name.blank?
       end
     end
   end
