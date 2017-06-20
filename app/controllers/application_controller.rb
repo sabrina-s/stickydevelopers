e60d88ed0a6bc623a2125c331faebfe541f29635
nndd91 <nndd91@hotmail.com>
2017-06-20 18:14:39 +0800

    Add user_cart methods, add cart dropdown to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :setup_cart
+
+  def setup_cart
+    unless @current_cart
+      @current_cart = current_user.build_user_cart
+    else
+      @current_cart = current_user.user_cart
+    end
+  end
 end
