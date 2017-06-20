4d09d26ffa906e6394be37e2ef70a9afe80ca6cc
nndd91 <nndd91@hotmail.com>
2017-06-20 13:57:23 +0800

    Add new fields to user_factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,9 +3,10 @@ FactoryGirl.define do
     # sequence(:username) { |n| "username#{n}" }
     sequence(:email) { |n| "email#{n}@example.com" }
     password "123123"
-    # firstname "John"
-    # lastname "Doe"
-    # birthdate "01-01-1990"
+    first_name "John"
+    last_name "Doe"
+    username "Sabrina"
+    birthdate "01-01-1990"
     # default_shipping ""
   end
 end
