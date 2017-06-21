9170b56c689fde957d72ab952644e40db0604a62
sabrina-s <tesmemepasla@gmail.com>
2017-06-21 13:34:52 +0800

    Add variation views

--- app/controllers/admin/variations_controller.rb
+++ app/controllers/admin/variations_controller.rb
@@ -1,27 +1,42 @@
 class Admin::VariationsController < ApplicationController
 
+  # def index
+  #   @variations = Variation.all
+  # end
+
   def new
     @variation = Variation.new
   end
 
   def create
+    @variation = Variation.new(variation_params)
+
     if @variation.save
-      redirect_to
+      redirect_to admin_product_path(slug: @product.slug)
     else
       render :new
     end
   end
 
   def edit
-
+    @variation = Variation.find(params[:id])
+    @product = Product.find_by(slug: params[:slug])
   end
 
   def update
+    @product = Product.find_by(slug: params[:slug])
+    @variation = Variation.find(params[:id])
 
+    @variation.update(variation_params)
+    if @variation.save
+      redirect_to admin_product_path(slug: @product.slug)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @variation.destroy
   end
 
   private
