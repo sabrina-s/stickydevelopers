59fa92bcd8b268869be83f388dd1ea68444ede0e
nndd91 <nndd91@hotmail.com>
2017-06-21 10:55:12 +0800

    Update routes with variations

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,24 @@ Rails.application.routes.draw do
 
   namespace :shop  do
     resources :products, only: [:index, :show], param: :slug
-    resource :cart, except: [:new, :create]
+    resource :cart, except: [:show, :create, :destroy]
     resources :orders
   end
 
   devise_for :admins
   namespace :admin do
-    resources :products, param: :slug
+    resources :products, param: :slug do
+      member do
+        resources :variation
+      end
+    end
   end
 
   # For Guests
   resources :products, only: [:index, :show]
   resource :cart, except: [:new, :create]
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
