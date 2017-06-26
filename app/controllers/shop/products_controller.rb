30ab8f2b7870bd66eeef9aaa0a4b5f6aed675cb9
sabrina-s <tesmemepasla@gmail.com>
2017-06-26 16:25:35 +0800

    Users can search through product database with the product's name

--- app/controllers/shop/products_controller.rb
+++ app/controllers/shop/products_controller.rb
@@ -1,11 +1,19 @@
 class Shop::ProductsController < ApplicationController
-  
+
   def index
     @products = Product.all
   end
 
   def show
     @product = Product.find_by(slug: params[:slug])
+  end
+
+  def search
+    @search = params[:search]
+
+    if params[:search]
+      @products = Product.search(params[:search])
+    end
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   has_many :variations, dependent: :destroy
+
+  def self.search(search)
+    where("name ILIKE ?", "%#{search}%")
+  end
 end
