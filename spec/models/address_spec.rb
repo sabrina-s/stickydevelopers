211652d10958680d462a2b3dbf05760e1d5077e3
nndd91 <nndd91@hotmail.com>
2017-06-26 20:03:44 +0800

    Edit validation from order to orders

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Address, type: :model do
   it { should belong_to(:user) }
-  it { should have_many(:order) }
+  it { should have_many(:orders) }
 
   it { should validate_presence_of(:line1) }
   it { should validate_presence_of(:zipcode) }
