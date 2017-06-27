26133826cd175256e27cae1a9766b8bac3177b88
nndd91 <nndd91@hotmail.com>
2017-06-27 10:37:18 +0800

    Add order relation to products through variation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   has_many :variations, dependent: :destroy
+  has_many :orders, through: :variations
 
   def self.search(search)
     where("name ILIKE ?", "%#{search}%")
--- app/models/variation.rb
+++ app/models/variation.rb
@@ -1,6 +1,8 @@
 class Variation < ApplicationRecord
   belongs_to :product
   has_many :user_cart_items, dependent: :destroy
+  has_many :orders, through: :user_cart_items
+
   validates :label, presence: true
   validates :price, presence: true, numericality: true
   validates :product, presence: true
