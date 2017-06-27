783cd4502039541319f53d892b3d625c1cfe040c
sabrina-s <tesmemepasla@gmail.com>
2017-06-27 14:59:05 +0800

    Orders controller spec (WIP)

---
+++ spec/controllers/admin/orders_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Admin::OrdersController, type: :controller do
+
+  describe 'GET #edit' do
+    
+    before do
+      get :edit
+    end
+
+  end
+  
+  describe 'PATCH #update' do 
+  end
+
+end
