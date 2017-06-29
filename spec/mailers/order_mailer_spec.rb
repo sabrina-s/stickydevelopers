5dc192cccb4e276735efb08daca6a98e671e6a1d
nndd91 <nndd91@hotmail.com>
2017-06-30 00:39:16 +0800

    Add rspec for order mailer

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:address) { create(:address, user: user) }
+  let(:order) { create(:order, user: user, address: address) }
+
+
+  describe 'order summary email' do
+    let(:mail) { OrderMailer.order_summary_email(user, order) }
+    it { expect(mail.subject).to eq('Your order summary') }
+    it { expect(mail.to).to eq([user.email]) }
+  end
+
+  describe 'order shipped email' do
+    let(:mail) { OrderMailer.order_shipped_email(user, order) }
+    it { expect(mail.subject).to eq('Your order is on the way!') }
+    it { expect(mail.to).to eq([user.email]) }
+  end
 end
