6f82507a9cc0ba3413fb42ebe0996260b3a2383d
nndd91 <nndd91@hotmail.com>
2017-06-29 10:29:58 +0800

    Add default url option for mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'stickydevelopers.herokuapp.com', port: 3000 }
+
   # SMTP settings for gmail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
